73ce37a90b10c0291563d9fd09934e4fd26599ee
baigang <baigang@localhost.localdomain>
2019-12-23 11:34:47 +0000

    微信

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -634,19 +634,19 @@
         wx.updateAppMessageShareData({
             title: '分享测试', // 分享标题
             desc: '描述', // 分享描述
-            link: '1905baigang.comcto.com/img/2ha.jpg', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: '1905baigang.comcto.com/img/2ha.jpg', // 分享图标
+            link: 'http://1905baigang.comcto.com/img/2ha.jpg', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: 'http://1905baigang.comcto.com/img/2ha.jpg', // 分享图标
             success: function () {
                 // 设置成功
                 alert(11111);
             }
         })
         //分享到盆友圈
-        wx.ready(function () {      //需在用户可能点击分享按钮前就先调用
+        wx.ready(function(){      //需在用户可能点击分享按钮前就先调用
             wx.updateTimelineShareData({
                 title: '分享测试', // 分享标题
-                link: '1905baigang.comcto.com/img/2ha.jpg', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                imgUrl: '1905baigang.comcto.com/img/2ha.jpg', // 分享图标
+                link: 'http://1905baigang.comcto.com/img/2ha.jpg', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'http://1905baigang.comcto.com/img/2ha.jpg', // 分享图标
                 success: function () {
                     alert("分享成功");
                 }
